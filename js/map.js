ad07b1ef612666b02a244195c5f5199ba9ac1cd2
skipokylas <skipokylas@gmail.com>
2017-08-07 15:17:23 +0300

    final

--- js/map.js
+++ js/map.js
@@ -1,8 +1,10 @@
-function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+ function initMap() {
+        var uluru = {lat:  49.843685, lng: 24.023840};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
+          zoom: 13,
+          center: uluru,
+          scrollwheel: false
+
         });
         var marker = new google.maps.Marker({
           position: uluru,
